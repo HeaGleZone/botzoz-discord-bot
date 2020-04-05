2a01677cd1a73bae32c9aebef28be56530805739
michaelarmani <michaelarmani00@gmail.com>
2020-04-05 13:57:14 +0200

    Improved commands with checks and error handling

--- cogs/commands_handler.py
+++ cogs/commands_handler.py
@@ -9,24 +9,40 @@ class CommandsHandler(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    @commands.has_permissions(administrator=True)
+    @commands.guild_only()
     async def shuffle(self, ctx):
         'Shuffles voice users between voice channels'
 
+        # Props
         author = ctx.message.author
-        isAdmin = author.guild_permissions.administrator
         user_guild = author.guild.id
         voice_channels = get_voice_channels(self.bot, user_guild)
         voice_users = get_voice_users(self.bot, user_guild)
 
-        if (isAdmin):
-            print(f'User {author} is shuffling voice members.')
-            await ctx.send('Shuffling voice users.')
-            for user in voice_users:
-                rnd_channel = random.choice(voice_channels)
-                await user.move_to(rnd_channel)
-        else:
+        # Response
+        print(f'User {author} is shuffling voice members.')
+        await ctx.send('Shuffling voice users.')
+        for user in voice_users:
+            rnd_channel = random.choice(voice_channels)
+            await user.move_to(rnd_channel)
+
+    @shuffle.error
+    async def general_guild_only_error(self, ctx, error):
+        # Props
+        author = ctx.message.author
+
+        # No DMs
+        if isinstance(error, commands.NoPrivateMessage):
             print(f'User {author} has no permissions to shuffle voice users.')
-            await ctx.send('You don\'t have permissions to do that.')
+            await ctx.send('Nope')
+            return
+
+        # Check permission
+        if isinstance(error, commands.MissingPermissions):
+            print(
+                f'User {author} has no permissions to run {ctx.message.content}.')
+            await ctx.send('You\'re not allowed to do that. Nice try.')
 
 
 def setup(bot):
